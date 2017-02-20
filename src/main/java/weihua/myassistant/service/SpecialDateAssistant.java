5d9f444cbd9cbaf1dbee2934905aed7938337cda
chengwei2 <chengwei2@JXWBJ1207261100.jiuxian.local>
2017-02-20 20:22:24 +0800

    新增天气、特殊日子助手服务

---
+++ src/main/java/weihua/myassistant/service/SpecialDateAssistant.java
@@ -0,0 +1,21 @@
+package weihua.myassistant.service;
+
+import weihua.myassistant.context.Context;
+import weihua.myassistant.request.RequestType;
+import weihua.myassistant.response.Response;
+
+/**
+ * 特殊日子助手服务
+ * 
+ * @author chengwei2
+ * 
+ */
+public class SpecialDateAssistant implements Assistant {
+
+	@Override
+	public Response getResponse(String request, RequestType requestType, Context context) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/weihua/myassistant/service/WetherAssistant.java
@@ -0,0 +1,21 @@
+package weihua.myassistant.service;
+
+import weihua.myassistant.context.Context;
+import weihua.myassistant.request.RequestType;
+import weihua.myassistant.response.Response;
+
+/**
+ * 天气助手服务
+ * 
+ * @author chengwei2
+ * 
+ */
+public class WetherAssistant implements Assistant {
+
+	@Override
+	public Response getResponse(String request, RequestType requestType, Context context) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
